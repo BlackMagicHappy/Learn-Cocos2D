9dcbc57995dfbeac0132f2a3df02e3a1c9942e50
BlackMagicHappy <BlackMagicHappy@users.noreply.github.com>
2014-09-06 20:33:59 +0800

    上传Hello world

---
+++ CCHelloWorld/CCHelloWorld/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  CCHelloWorld
+//
+//  Created by heping on 14-9-4.
+//  Copyright Huawei 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
